7a185cbbea496b7a78db23948c58d1cce2891637
Amir kalid <amirkjaled@gmail.com>
2026-04-06 17:27:59 +0300

    update

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -38,6 +38,15 @@ const projects = [
     link: "https://shopify-sable-tau.vercel.app/",
     github: "#",
   },
+    {
+    title: "Travel planner",
+    description:
+      "The best way to manage and share your travel plans with friends and family.",
+    image: "/projects/project5.png",
+    tags: ["Next.js", "postgresql", "NextAuth", "react-globegl"],
+    link: "https://travel-nu-mauve.vercel.app/",
+    github: "#",
+  },
 ]
 
 
@@ -58,7 +67,7 @@ export const Projects = () => {
                     <div key={idx} className="group glass rounded-2xl overflow-hidden animate-fade-in md:row-span-1" style={{animationDelay: `${(idx + 1) * 100}ms`}}>
                         <div className="overflow-hidden relative aspect-video">
                             <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"/>
-                             <div className="absolute inset-0 bg-gradient-to-t from-card via-card/50 to-transparent opacity-60" />
+                             <div className="absolute inset-0 bg-linear-to-t from-card via-card/50 to-transparent opacity-60" />
                              <div className="absolute inset-0 gap-4 items-center justify-center flex opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                                 <a href={project.link} className="rounded-full glass p-3 hover:text-primary-foreground hover:bg-primary transition-all"><ArrowUpRight className="h-5 w-5"/></a>
                                 <a href={project.link} className="rounded-full glass p-3 hover:text-primary-foreground hover:bg-primary transition-all"><Github className="h-5 w-5"/></a>
